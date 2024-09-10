7d1fd11401943512188ea9673935ac89d03d10f5
ezras256 <120516359+ezras256@users.noreply.github.com>
2024-09-10 14:22:05 +0000

    Created 'familias' page

--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request): 
     return render(request, 'home/index.html')
+
+def families(request): 
+    return render(request, 'home/families.html')
